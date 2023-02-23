317be4b93f67c45c012d06895230d9e0947f3b7e
yufrolov <yuriifrolov39@gmail.com>
2023-02-23 13:43:34 +0300

    Spring-boot project setup

---
+++ src/main/java/com/yufrolov/cryptocurrencyexchange/CryptocurrencyExchangeApplication.java
@@ -0,0 +1,13 @@
+package com.yufrolov.cryptocurrencyexchange;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CryptocurrencyExchangeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CryptocurrencyExchangeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yufrolov/cryptocurrencyexchange/CryptocurrencyExchangeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yufrolov.cryptocurrencyexchange;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CryptocurrencyExchangeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
